0e4c0b7f3d903074726ecbe04ef3da0df86cdead
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-08 01:33:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,9 @@ class Transfer
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      #@status = "complete"
+      @status = "complete"
     else 
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
